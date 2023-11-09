98f59431a47b1044b7b0d702574d5a4b46197043
Spaikyjordi <144990855+Spaikyjordi@users.noreply.github.com>
2023-11-09 08:23:04 +0100

    Add files via upload

---
+++ Arduino/name-using-serial.begin.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  // Print name to the Serial Monitor
+  Serial.println("Hello World");
+  // Delay for a while to make the output readable
+  delay(1000);  // 1 second delay
+}
