ad16e0a6748967b9da1c2f49578e693834b7a4e8
Spaikyjordi <144990855+Spaikyjordi@users.noreply.github.com>
2024-02-18 16:44:09 +0100

    Update Triangulo.java

--- Java/Triangulo.java
+++ Java/Triangulo.java
@@ -1,44 +1,32 @@
-//Pitagoras
+// triangulo pitagoras en java
+public class Main {  
+   public static void main(String[] args) {
+      double hypotenuseA, catetoB, catetoC, area, perimeter;
+      catetoB = 4; //change this to your values
+      catetoC = 6; //change this to your values
+   
+      hypotenuseA = Math.sqrt(catetoB*catetoB + catetoC*catetoC);
+      
+      // Print results
+      System.out.print("The side missing is ");  // Print description
+      System.out.println(hypotenuseA);          // Print the value stored in the variable
 
-public class Main {
-
-public static void main(String[] args) {
-
-double hypotenuseA, catetoB, catetoC, area, perimeter;
-
-catetoB = 4; //change this to your values
-
-catetoC = 6; //change this to your values
-
-hypotenuseA = Math.sqrt(catetoB*catetoB + catetoC*catetoC);
-
-// Print result
-
-System.out.print("The side missing is ");
-
-System.out.println(hypotenuseA);
-
+   }
 }
 
-}
+// Area 
 
-// Area
+public class Main {   
 
-public class Main {
+   public static void main(String[] args) { 
 
-public static void main(String[] args) {
+      double area, base, height; 
+      base=12; 
+      height=15; 
+      area = (base * height)/2; 
+      System.out.print("The side area is ");    
+      System.out.println(area);  
 
-double area, base, height;
+      } 
 
-base=12;
-
-height=15;
-
-area = (base * height)/2;
-
-System.out.print("The side area is ");
-
-System.out.println(area);
-
-}
-}
+  } 
