dd3a42ab3aed16ee337dc4b6c5253b56c896185c
Spaikyjordi <144990855+Spaikyjordi@users.noreply.github.com>
2024-02-18 16:44:30 +0100

    Update 5-12-2024.java

--- Java/5-12-2024.java
+++ Java/5-12-2024.java
@@ -1,47 +1,25 @@
 // serial.print oneliner
+  System.out.print("The sum, difference, product, quotient and remainder of "+ number1 + "and" + number2 + "are" + sum + "," + difference +
+  "," + product + "," + quotient + ", and" + remainder);  
 
-System.out.print("The sum, difference, product, quotient and remainder of "+ number1 + "and" + number2 + "are" + sum + "," + difference +
-
-"," + product + "," + quotient + ", and" + remainder);
-
-// sum of two numbers
-
-public class Main {
-
-public static void main(String[] args) {
-
-double number1 = 99;
-
-double number2 = 4;
-
-double sum = 0;
-
-sum = (number1*31)+(number2*17);
-
-System.out.println(sum);
-
-}
-
-}
-
-// same but adding another number that is 77
-
-public class Main {
-
-public static void main(String[] args) {
-
-double number1 = 99;
-
-double number2 = 4;
-
-int number3 = 77;
-
-double sum = 0;
-
-sum = (number1*31)+(number2*17)+number3;
-
-System.out.println(sum);
-
-}
-
+// sum of two numbers 
+      public class Main {  
+   public static void main(String[] args) {
+      double number1 = 99;
+      double number2 = 4;
+      double sum = 0;
+      sum = (number1*31)+(number2*17);
+      System.out.println(sum);
+   }
+} 
+// same but adding another number that is 77 
+      public class Main {  
+   public static void main(String[] args) {
+      double number1 = 99;
+      double number2 = 4;
+      int number3 = 77;
+      double sum = 0;
+      sum = (number1*31)+(number2*17)+number3;
+      System.out.println(sum);
+   }
 }
